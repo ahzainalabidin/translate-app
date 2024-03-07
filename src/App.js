d2d0d86923b49c15270494db30bd4b7f1a913664
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-07 10:02:10 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Field from "./components/field";
+import Translate from "./components/translate";
+import Languages from "./components/languages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Hi there!</h3>
     </div>
   );
 }
